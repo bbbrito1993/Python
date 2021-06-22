71cee8dc91a8d4e35f07a97983d9aa29a9bd1c60
bbbrito1993 <86316871+bbbrito1993@users.noreply.github.com>
2021-06-22 17:14:44 -0300

    Add files via upload

---
+++ aula03.py
@@ -0,0 +1,58 @@
+a = int(input('Primeiro Bimestre: '))
+if a > 10:
+    a = int(input('Volores Digitados Incorretamente.Digite novamente. Primeiro Bimestre: '))
+b = int(input('Segundo Bimestre: '))
+if b > 10:
+    b = int(input('Volores Digitados Incorretamente.Digite novamente. Segundo Bimestre: '))
+c = int(input('Terceiro Bimestre: '))
+if c > 10:
+    c = int(input('Volores Digitados Incorretamente.Digite novamente. Terceiro Bimestre: '))
+d = int(input('Quarto Bimestre: '))
+if d > 10:
+    d = int(input('Volores Digitados Incorretamente.Digite novamente. Quarto Bimestre: '))
+media = (a + b + c + d) / 4
+print('A média do aluno é : {}'.format(media))
+
+
+
+
+# Utilizando or not
+
+# a = int(input('Entre com o primeio valor: '))
+# b = int(input('Entre com o primeio valor: '))
+# resto_a = a % 2
+# resto_b = b % 2
+#
+# if resto_a == 0 or not resto_b > 0:
+#     print('Um dos valores digitados é par!')
+# else:
+#     print('Nenhum dos valores digitados é par!')
+
+
+
+# Utilizando or
+
+# a = int(input('Entre com o primeio valor: '))
+# b = int(input('Entre com o primeio valor: '))
+# resto_a = a % 2
+# resto_b = b % 2
+#
+# if resto_a ==0 or resto_b == 0:
+#     print('Um dos valores digitados é par!')
+# else:
+#     print('Nenhum dos valores digitados é par!')
+
+# Utiliazando if / else / elif
+
+# a = int(input('Primeiro Valor: '))
+# b = int(input('Segundo Valor: '))
+# c = int(input('Terceiro Valor: '))
+#
+# if a > b and a > c:
+#     print('O maior valor é: {}'.format(a))
+# elif b > a and b > c:
+#     print('O maior valor é: {}'.format(b))
+# else:
+#     print('O maior valor é: {}'.format(c))
+#
+#     print('Final do Programa')
